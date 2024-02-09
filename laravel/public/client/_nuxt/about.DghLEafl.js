e03b025f00a33a1203c3751afc659e60b1705a29
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2024-02-10 01:05:39 +0300

    final

---
+++ laravel/public/client/_nuxt/about.DghLEafl.js
@@ -0,0 +1,1 @@
+import{c as t,a as e,t as a,o}from"./entry.1NNChIZa.js";const s="About Page",u={__name:"about",setup(c){return(n,r)=>(o(),t("div",null,[e("h1",null,a(s))]))}};export{u as default};
---
+++ laravel/public/client/_nuxt/index.nR3Acep8.js
@@ -0,0 +1,1 @@
+import{c as e,a as t,t as a,o as n}from"./entry.1NNChIZa.js";const o="Index Page",_={__name:"index",setup(s){return(c,r)=>(n(),e("div",null,[t("h1",null,a(o))]))}};export{_ as default};
