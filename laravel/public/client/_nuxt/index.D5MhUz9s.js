a0dbfd7d30806d7daa11c115c6221ea7dd780689
akramghaleb <46090581+akramghaleb@users.noreply.github.com>
2024-02-11 09:08:41 +0300

    add /public/client to .gitignore

--- laravel/public/client/_nuxt/index.D5MhUz9s.js
+++
@@ -1,1 +0,0 @@
-import{c as e,a as t,t as a,o as n}from"./entry.OLdbExHX.js";const o="Index Page",_={__name:"index",setup(s){return(c,r)=>(n(),e("div",null,[t("h1",null,a(o))]))}};export{_ as default};
