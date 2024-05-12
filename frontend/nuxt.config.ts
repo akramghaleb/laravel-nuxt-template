0c9922065c6e9875ed47798952075aec84b6ade3
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2024-05-12 06:31:23 +0300

    frontend installing pinia + nuxt config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  ssr: true,
+  devtools: { enabled: true },
+
+  modules: ["@pinia/nuxt", '@pinia-plugin-persistedstate/nuxt'],
+  imports: {
+    dirs: ['./stores'],
+  },
+
+  pinia: {
+    autoImports: ['defineStore', 'acceptHMRUpdate'],
+  },
+  nitro: {
+    output: {
+      publicDir: '../backend/public/app/'
+    }
+  },
 })
